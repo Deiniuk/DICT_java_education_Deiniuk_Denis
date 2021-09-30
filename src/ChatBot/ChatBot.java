e8ba24c3f2ed2ecd4c9816581d12eb81eb04f118
Deiniuk <d.s.deiniuk@student.khai.edu>
2021-09-30 15:01:19 +0300

    ChatBot 5-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -12,19 +12,26 @@ public class ChatBot {
         String user = scan.nextLine();
         System.out.println("What a great name you have, " + user + "!\nLet me guess your age.\n" +
                 "Enter remainders of dividing your age by 3, 5 and 7.");
-        Scanner scan1 = new Scanner(System.in);
-        int remainder3 = scan1.nextInt();
-        Scanner scan2 = new Scanner(System.in);
-        int remainder5 = scan2.nextInt();
-        Scanner scan3 = new Scanner(System.in);
-        int remainder7 = scan3.nextInt();
+        int remainder3 = scan.nextInt();
+        int remainder5 = scan.nextInt();
+        int remainder7 = scan.nextInt();
         int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
         System.out.println("Your age is " + age + " that`s a good time to start programming!");
         System.out.println("Now I will prove to you that I can count to any number you want.");
-        Scanner scan4 = new Scanner(System.in);
-        int number = scan4.nextInt();
+        int number = scan.nextInt();
         for (int i=0; i <= number; i++) {
             System.out.println(i + "!");
         }
+        int hi = 0;
+        do {
+            System.out.println("Давай проверим насколько ты умён!\nКто ты?\n1. Amogus\n2. Человек\n3. Пёсик\n4. " + name);
+            int answer = scan.nextInt();
+            if(answer == 4) {
+                System.out.println(name + ", ты молодец. Пока!");
+                hi = 1;
+            } else {
+                System.out.println("Попробуй ещё раз!");
+            }
+        } while(hi == 0);
     }
 }
