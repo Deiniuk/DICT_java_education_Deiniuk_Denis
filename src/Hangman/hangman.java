46bfaedcf115a4e4a7d2553ed9e0d410967e7b45
Deiniuk <d.s.deiniuk@student.khai.edu>
2021-10-21 16:41:00 +0300

    Completed 4-stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class hangman {
     public static void main(String[] args) {
-        String[] word = new String[] {"java", "kotlin", "python", "javascript"};
+        final String[] word = new String[] {"java", "kotlin", "python", "javascript"};
         Random random = new Random();
         int index = random.nextInt(word.length);
         String randomWord = word[index];
-        System.out.print("HANGMAN\n" +
-                "The game will be available soon.\n" +
-                "Guess the word: ");
+        char[] typo = randomWord.toCharArray();
+        for (int i = 2; i < typo.length; i++) {
+            typo[i] = '-';
+        }
+        String s = String.valueOf(typo);
+        System.out.print("...HANGMAN...\n" +
+                "Guess the word " + s + ": ");
         Scanner scan = new Scanner(System.in);
         String user = scan.nextLine();
         if (randomWord.equals(user)) {
