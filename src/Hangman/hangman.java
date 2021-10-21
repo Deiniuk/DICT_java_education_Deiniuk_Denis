b22ce288321821f0985eec9400fb6e4b5f7b9059
Deiniuk <d.s.deiniuk@student.khai.edu>
2021-10-21 14:11:18 +0300

    Completed 2-stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,8 +1,18 @@
 package Hangman;
+import java.util.Scanner;
 
 public class hangman {
     public static void main(String[] args) {
-        System.out.println("HANGMAN\n" +
-                "The game will be available soon.");
+        String word = "java";
+        System.out.print("HANGMAN\n" +
+                "The game will be available soon.\n" +
+                "Guess the word: ");
+        Scanner scan = new Scanner(System.in);
+        String user = scan.nextLine();
+        if (word.equals(user)) {
+            System.out.println("You survive!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
