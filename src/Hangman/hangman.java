06b8ce26dadd460dd68252f8cb4988c02b05fe34
Deiniuk <d.s.deiniuk@student.khai.edu>
2021-10-21 14:26:19 +0300

    Completed 3-stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,15 +1,19 @@
 package Hangman;
+import java.util.Random;
 import java.util.Scanner;
 
 public class hangman {
     public static void main(String[] args) {
-        String word = "java";
+        String[] word = new String[] {"java", "kotlin", "python", "javascript"};
+        Random random = new Random();
+        int index = random.nextInt(word.length);
+        String randomWord = word[index];
         System.out.print("HANGMAN\n" +
                 "The game will be available soon.\n" +
                 "Guess the word: ");
         Scanner scan = new Scanner(System.in);
         String user = scan.nextLine();
-        if (word.equals(user)) {
+        if (randomWord.equals(user)) {
             System.out.println("You survive!");
         } else {
             System.out.println("You lost!");
