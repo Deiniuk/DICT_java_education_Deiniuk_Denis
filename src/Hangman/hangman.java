a6374ff18d30a855acdfe3fed028ecf9a31997c3
Deiniuk <d.s.deiniuk@student.khai.edu>
2021-10-25 12:01:25 +0300

    Completed 5-stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,26 +1,48 @@
 package Hangman;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class hangman {
     public static void main(String[] args) {
         final String[] word = new String[] {"java", "kotlin", "python", "javascript"};
+        ArrayList<String> letters = new ArrayList<>();
         Random random = new Random();
-        int index = random.nextInt(word.length);
-        String randomWord = word[index];
-        char[] typo = randomWord.toCharArray();
-        for (int i = 2; i < typo.length; i++) {
-            typo[i] = '-';
+        String randomWord = word[random.nextInt(word.length)];
+        char[] hypo = randomWord.toCharArray();
+        StringBuffer s = new StringBuffer(randomWord);
+        for (int i = 0; i < hypo.length; i++) {
+            s.setCharAt(i, '-');
         }
-        String s = String.valueOf(typo);
-        System.out.print("...HANGMAN...\n" +
-                "Guess the word " + s + ": ");
+        System.out.print("...HANGMAN...\n");
         Scanner scan = new Scanner(System.in);
-        String user = scan.nextLine();
-        if (randomWord.equals(user)) {
-            System.out.println("You survive!");
-        } else {
-            System.out.println("You lost!");
+        boolean boolTrue = true;
+        short attempt = 8;
+        while (boolTrue) {
+            if (attempt > 0) {
+                System.out.print("\n" + s + "\nInput a letter:");
+                String user = scan.nextLine();
+                letters.add(user);
+                attempt--;
+                if (randomWord.contains(user)) {
+                    for (int i = 0; i < randomWord.length(); i++) {
+                        if (hypo[i] == user.charAt(0)) {
+                            s.setCharAt(i, user.charAt(0));
+                        }
+                    }
+                    if (s.toString().equals(randomWord)) {
+                        System.out.println("Thanks for playing!\n" +
+                                "We'll see how well you did in the next stage");
+                        boolTrue = false;
+                    }
+                } else {
+                    System.out.println("That letter doesn't appear in the word");
+                }
+            } else {
+                System.out.println("Thanks for playing!\n" +
+                        "We'll see how well you did in the next stage");
+                boolTrue = false;
+            }
         }
     }
 }
