de387484544343d43d26febe8e8b838ce2f1b225
Deiniuk <d.s.deiniuk@student.khai.edu>
2021-12-08 19:13:51 +0200

    Stage 5 completed

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,17 @@
-/*public static void main(String[] args) {
-        System.out.println("Write how many ml of water the coffee machine has:");
-        Scanner scan = new Scanner(System.in);
-        int water = scan.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milk = scan.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int beans = scan.nextInt();
-        System.out.println("Write how many cups of coffee you will need:");
-        int cups = scan.nextInt();
-        int nowWater = (int)water/200;
-        int nowMilk = (int)milk/50;
-        int nowBeans = (int)beans/15;
-        int[] manyCaps = {nowWater, nowMilk, nowBeans};
-        Arrays.sort(manyCaps);
-        int cf = manyCaps[0];
-        if(cups == cf) {
-        System.out.println("Yes, I can make that amount of coffee");
-        } else {
-        if (cups > cf) {
-        System.out.println("No, I can make only "+ cf + " cups of coffee.");
-        } else {
-        if (cups < cf) {
-        System.out.println("Yes, I can make that amount of coffee (and even " + (cf-cups) + " more than that)");
-        }
-        }
-        }
+/*if(scan.nextLine().equals("back")) {
+            menu();
+        }else {
+            switch (scan.nextInt()) {
+                case 1 -> {
+                    espresso();
+                }
+                case 2 -> {
+                    latte();
+                }
+                case 3 -> {
+                    cappuccino();
+                }
+                default -> System.out.println("\n ERROR ");
+            }
         }
 */
