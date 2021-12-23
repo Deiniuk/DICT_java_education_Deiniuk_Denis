8e04151f6fcda80fc721109e484d288c60877c69
Deiniuk <d.s.deiniuk@student.khai.edu>
2021-12-23 12:38:47 +0200

    Stage 3 complete

--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -8,21 +8,86 @@ public class Matrix {
 
         int rows;
         int columns;
-        System.out.println("Enter size of the matrix: ");
-        rows = sc.nextInt();
-        columns = sc.nextInt();
 
-        System.out.println("Enter matrix: ");
+        while (true) {
+            System.out.println("1. Add matrices\r\n" +
+                    "2. Multiply matrix to a constant\r\n" +
+                    "3. Multiply matrices\r\n" +
+                    "0. Exit");
 
-        int[][] matMultiply = fillMatrix(rows,columns);
+            int choice = sc.nextInt();
 
-        System.out.println("Enter constonant: ");
 
-        int n = sc.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Enter size of first matrix: ");
+                    rows = sc.nextInt();
+                    columns = sc.nextInt();
+                    System.out.println("Enter matrix: ");
+                    int[][] mat1 = fillMatrix(rows, columns);
+                    System.out.println("Enter size of second matrix: ");
+                    rows = sc.nextInt();
+                    columns = sc.nextInt();
+                    System.out.println("Enter second matrix: ");
+                    int[][] mat2 = fillMatrix(rows, columns);
+                    System.out.println("The result is: ");
+                    sumUpMatrix(mat1, mat2, rows, columns);
+                }
+                case 2 -> {
+                    System.out.println("Enter size of the matrix: ");
+                    rows = sc.nextInt();
+                    columns = sc.nextInt();
+                    System.out.println("Enter matrix: ");
+                    int[][] matMultiply = fillMatrix(rows, columns);
+                    System.out.println("Enter constonant: ");
+                    int n = sc.nextInt();
+                    System.out.println("The result is: ");
+                    multiplyMatrix(matMultiply, rows, columns, n);
+                }
+                case 3 -> {
+                    System.out.println("Enter size of first matrix: ");
+                    rows = sc.nextInt();
+                    columns = sc.nextInt();
+                    System.out.println("Enter first matrix: ");
+                    int[][] mat1p = fillMatrix(rows, columns);
+                    System.out.println("Enter size of second matrix: ");
+                    int rows2 = sc.nextInt();
+                    int columns2 = sc.nextInt();
+                    System.out.println("Enter second matrix: ");
+                    int[][] mat2p = fillMatrix(rows2, columns2);
+                    System.out.println("The multiplication result is:");
+                    multiplyMatrices(mat1p, mat2p);
+                }
+                default -> System.exit(0);
+            }
+        }
+    }
+    public static void sumUpMatrix(int[][] firstMatrix,int[][] secondMatrix,int rows, int columns) {
 
-        System.out.println("The result is: ");
+        int[][] sum = new int[rows][columns];
+        for(int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                sum[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+            }
+        }
 
-        multiplyMatrix(matMultiply,rows,columns,n);
+        printMatrix(sum);
+    }
+
+    public static void multiplyMatrices(int[][] a, int[][]b) {
+        int rowsInA = a.length;
+        int columnsInA = a[0].length;
+        int columnsInB = b[0].length;
+        int[][] c = new int[rowsInA][columnsInB];
+        for (int i = 0; i < rowsInA; i++) {
+            for (int j = -0; j < columnsInB; j++) {
+                c[i][j] = 0;
+                for (int k = 0; k < columnsInA; k++) {
+                    c[i][j] = c[i][j] + a[i][k] * b[k][j];
+                }
+            }
+        }
+        printMatrix(c);
     }
 
     public static void multiplyMatrix(int[][] firstMatrix,int rows, int columns,int c) {
@@ -35,6 +100,7 @@ public class Matrix {
 
         printMatrix(pr);
     }
+
     public static int[][] fillMatrix(int rows, int columns) {
         Scanner sc = new Scanner(System.in);
 
@@ -48,6 +114,7 @@ public class Matrix {
 
         return firstMatrix;
     }
+
     public static void printMatrix(int[][] pr) {
         for(int[] row : pr) {
             for (int column : row) {
